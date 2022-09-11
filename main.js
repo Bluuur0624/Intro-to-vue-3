59f9992761f164d859fe516a776602e9e4a8b1d7
Bluuur0624 <OasYs1068@gmail.com>
2022-09-11 21:14:02 +0800

    L3 done

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data: function(){
         return{
             product: 'Socks',
-            description: 'warm, comfy, feel like home'
+            description: 'warm, comfy, feel like home',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://wavu.wiki/t/Main_Page'
         }
     }
 })
