ece157b3d7488bb4815883f9c771c5c8bb7aa4d9
Bluuur0624 <OasYs1068@gmail.com>
2022-09-11 21:01:35 +0800

    L2 completed

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function(){
+        return{
+            product: 'Socks',
+            description: 'warm, comfy, feel like home'
+        }
+    }
+})
