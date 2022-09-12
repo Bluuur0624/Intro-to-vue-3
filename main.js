d57e929d12e31f810fb5f46343589954a9909bd8
Bluuur0624 <OasYs1068@gmail.com>
2022-09-12 08:30:33 +0800

    L6 done

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 const app = Vue.createApp({
     data: function(){
         return{
+            cart: 0,
             product: 'Socks',
+            image:'./assets/images/socks_green.jpg',
             description: 'warm, comfy, feel like home',
-            image: './assets/images/socks_blue.jpg',
             url: 'https://wavu.wiki/t/Main_Page',
             inventory: 100,
             onSale: true,
             details:['50% cotton','30% wool','20% polyester'],
             variants:[
-                {id:2234, color:'green'},
-                {id:2235, color:'blue'},
+                {id:2234, color:'green', image:'./assets/images/socks_green.jpg'},
+                {id:2235, color:'blue', image: './assets/images/socks_blue.jpg'},
             ],
             sizes:['S','M','L','XL']
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1
+        },
+        removeFromCart(){
+            this.cart -= 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
